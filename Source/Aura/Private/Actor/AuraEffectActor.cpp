0bd755958c9125cdc5966ef6fe0a2bbda66f5172
DTIS007 <1908029030@qq.com>
2024-02-25 16:07:06 +0800

    Finish the Side Quest: Make the actor can apply a series of GE, and Remove the infinite GE one by one. This made me feel so cool

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -32,7 +32,14 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	const FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+
+	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite == true && InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		ActiveEffectHandles.Add(ActiveEffectHandle, TargetASC);
+	}
+	
 	
 }
 
@@ -46,6 +53,10 @@ void AAuraEffectActor::OnOverlap(AActor* TargetActor)
 	{
 		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
 	}
+	if (InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
 }
 
 void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
@@ -58,5 +69,137 @@ void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
 	{
 		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
 	}
+	if (InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+	if (InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if (!IsValid(TargetASC)) return;
+
+		TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+		// 还需要找到对应的 ASC和 FActiveGameplayEffectHandle 句柄，两者一定是对应的。
+		for (auto HandlePair : ActiveEffectHandles)
+		{
+			// ActiveEffectHandles 的键值对是 FActiveGameplayEffectHandle : ASC
+			if (TargetASC == HandlePair.Value)
+			{
+				// RemoveActiveGameplayEffect函数还可以控制移除的 Stacks 的数量，默认是移除全部，我们这里只移除一个 Stack 
+				TargetASC->RemoveActiveGameplayEffect(HandlePair.Key, 1);
+				// 移除 GE 后，这对存储的键值对数据也不需要了，移除即可。记住不要在循环里面删除，不然会导致程序崩溃，在循环体外进行元素移除
+				HandlesToRemove.Add(HandlePair.Key);
+			}
+		}
+		// 移除 GE 后，这对存储的键值对数据也不需要了，移除即可。记住不要在循环里面删除，不然会导致程序崩溃，在循环体外进行元素移除
+		for (auto& Handle :HandlesToRemove)
+		{
+			ActiveEffectHandles.FindAndRemoveChecked(Handle);
+		}
+	}
+}
+
+void AAuraEffectActor::OnOverlapExpend(AActor* TargetActor)
+{
+	// 遍历 InstantGEs 容器，然后依次 Apply GE
+	for (const auto InstantGE : InstantGEs)
+	{
+		// 判断 EEffectApplicationPolicy 类型
+		if (InstantGE.Value == EEffectApplicationPolicy::ApplyOnOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, InstantGE.Key);
+		}
+	}
+	// 遍历 DurationGEs 容器，然后依次 Apply GE
+	for (const auto DurationGE : DurationGEs)
+	{
+		// 判断 EEffectApplicationPolicy 类型
+		if (DurationGE.Value == EEffectApplicationPolicy::ApplyOnOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, DurationGE.Key);
+		}
+	}
+	// 遍历 InfiniteGEs 容器，然后依次 Apply GE
+	for (const auto InfiniteGE : InfiniteGEs)
+	{
+		if (InfiniteGE.InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, InfiniteGE.InfiniteGameplayEffectClass);
+		}
+	}
+}
+
+void AAuraEffectActor::OnEndOverlapExpend(AActor* TargetActor)
+{
+	// 遍历 InstantGEs 容器，然后依次 Apply GE
+	for (const auto InstantGE : InstantGEs)
+	{
+		// 判断 EEffectApplicationPolicy 类型
+		if (InstantGE.Value == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, InstantGE.Key);
+		}
+	}
+	// 遍历 DurationGEs 容器，然后依次 Apply GE
+	for (const auto DurationGE : DurationGEs)
+	{
+		// 判断 EEffectApplicationPolicy 类型
+		if (DurationGE.Value == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, DurationGE.Key);
+		}
+	}
+	// 遍历 InfiniteGEs 容器，然后依次 Apply GE
+	for (const auto InfiniteGE : InfiniteGEs)
+	{
+		if (InfiniteGE.InfiniteEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		{
+			// 应用 GE
+			ApplyEffectToTarget(TargetActor, InfiniteGE.InfiniteGameplayEffectClass);
+		}
+	}
+
+	/**
+	 * 移除 InfiniteGEs 的逻辑：
+	 *		存储 InfiniteGEs 的 FActiveGameplayEffectHandle 对象不能存储在 TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> 对象中
+	 *		因为这些 InfiniteGEs 都是存储在同一个 Actor 上，同一个 Actor 变量获取的 UAbilitySystemComponent 肯定是相同的，所以无法作为不同 GE 的唯一标识。
+	 *		
+	 * 
+	 */
+	// 遍历 InfiniteGEs 容器，然后依次移除 GE
+	for (const auto InfiniteGE : InfiniteGEs)
+	{
+		if (InfiniteGE.InfiniteEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+		{
+			UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+			if (!IsValid(TargetASC)) return;
+
+			TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+			// 还需要找到对应的 ASC和 FActiveGameplayEffectHandle 句柄，两者一定是对应的。
+			for (auto HandlePair : ActiveEffectHandles)
+			{
+				// ActiveEffectHandles 的键值对是 FActiveGameplayEffectHandle : ASC
+				if (TargetASC == HandlePair.Value)
+				{
+					// RemoveActiveGameplayEffect函数还可以控制移除的 Stacks 的数量，默认是移除全部，我们这里只移除一个 Stack 
+					TargetASC->RemoveActiveGameplayEffect(HandlePair.Key, 1);
+					// 移除 GE 后，这对存储的键值对数据也不需要了，移除即可。记住不要在循环里面删除，不然会导致程序崩溃，在循环体外进行元素移除
+					HandlesToRemove.Add(HandlePair.Key);
+				}
+			}
+			// 移除 GE 后，这对存储的键值对数据也不需要了，移除即可。记住不要在循环里面删除，不然会导致程序崩溃，在循环体外进行元素移除
+			for (auto& Handle :HandlesToRemove)
+			{
+				ActiveEffectHandles.FindAndRemoveChecked(Handle);
+			}
+		}
+	}// for (const auto InfiniteGE : InfiniteGEs)
+
+	
 }
 
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include  "GameplayEffectTypes.h"
 #include "AuraEffectActor.generated.h"
 
+class UAbilitySystemComponent;
 class UGameplayEffect;
 
 /**
@@ -27,6 +29,21 @@ enum class EEffectRemovalPolicy
 {
 	RemoveOnEndOverlap,
 	DoNotRemove
+};
+
+USTRUCT(BlueprintType)
+struct FInfiniteGE
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	EEffectApplicationPolicy InfiniteEffectApplicationPolicy= EEffectApplicationPolicy::DoNotApply;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+
+	
 };
 
 UCLASS()
@@ -62,7 +79,7 @@ protected:
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
-	EEffectApplicationPolicy DurationEffectApplicationPolicy= EEffectApplicationPolicy::DoNotApply;
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 	
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
@@ -72,4 +89,51 @@ protected:
 
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
 	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+	
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
+
+	/**
+	 * Side Quest：制作一个功能，一个Actor可以应用一系列的 GE
+	 * 思路：
+	 *	1. 针对 Instant GE：
+	 *		- 创建一个 TMap<TSubclassOf<UGameplayEffect>, EEffectApplicationPolicy> InstantGEs 用于存储 Actor 所需要触发的所有 GE
+	 *		- 随后在 OnOverlap 函数中一个个调用 ApplyEffectToTarget 函数即可
+	 *	2. 针对 Duration GE：
+	 *		- 同样创建 TMap<DurationGameplayEffectClass, DurationEffectApplicationPolicy> DurationGEs 用于存储 Actor 所需要触发的所有 GE
+	 *		- 同样 OnOverlap 函数中一个个调用 ApplyEffectToTarget 函数即可
+	 *	3. 针对 Infinite GE：
+	 *		- 次情况和之前的不太一样，因为 Infinite GE 需要移除，移除时需要依赖于之前保存的 FActiveGameplayEffectHandle 类型的变量。
+	 *		- 而且 Infinite GE 涉及三个需要存储的变量 InfiniteGameplayEffectClass, InfiniteEffectApplicationPolicy, InfiniteEffectRemovalPolicy
+	 *		- 所以这里用 TMap 就无法存储了，这里创建一个结构体数据进行存储 
+	 *		
+	 */
+
+	/**
+	 * 不在之前的函数中直接修改，而是选择重新制作 OnOverlap 和 OnEndOverlap 函数用于测试，这样就不会影响之前代码的逻辑
+	 */
+	UFUNCTION(BlueprintCallable)
+	void OnOverlapExpend(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlapExpend(AActor* TargetActor);
+	
+	/**
+	 * 1. Instant GE
+	 */
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	TMap<TSubclassOf<UGameplayEffect>, EEffectApplicationPolicy> InstantGEs;
+	
+	
+	/**
+	 * 2. Duration GE
+	 */
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	TMap<TSubclassOf<UGameplayEffect>, EEffectApplicationPolicy> DurationGEs;
+	
+	
+	/**
+	 * 3. Infinite GE
+	 */
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Applied Effects")
+	TArray<FInfiniteGE> InfiniteGEs;
 };
